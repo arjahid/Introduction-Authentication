269d437402ed70f9487296f4b7c810080e51dd7b
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-02-11 01:18:34 +0600

    added authentication

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase.init";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [error, setError] = useState("");
@@ -47,14 +48,11 @@ const SignUp = () => {
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+         
+         
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <h1 className="text-5xl font-bold">Sign UP now!</h1>
           <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
               <label className="label">
@@ -110,6 +108,7 @@ const SignUp = () => {
               User created successfully
             </div>
           )}
+          <p>already have an account please <Link to='/login'>Login</Link></p>
         </div>
       </div>
     </div>
