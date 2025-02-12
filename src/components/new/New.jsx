f38c64f77f25240b45255fe72d321dc22b526f6e
arjahid <jhjahidhasanar@gmail.com>
2025-02-12 21:11:27 +0600

    added

---
+++ src/components/new/New.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const New = () => {
+    return (
+        <div>
+            <h2>I am form new</h2>
+        </div>
+    );
+};
+
+export default New;
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import SignUp from './components/SignUp/SignUp'
+import New from './components/new/New'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -26,13 +27,18 @@ const router = createBrowserRouter([
         element:<Login></Login>
       },
       {
+        path:'new',
+        element:<New></New>
+      },
+      {
         path:'register',
         element:<Register></Register>
       },
       {
         path:'signup',
         element:<SignUp></SignUp>
-      }
+      },
+      
     ]
   }
 ])
